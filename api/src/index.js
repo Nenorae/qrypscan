cbba3b2947d5ef033b53c993f43cc98d369bc3c4
Nenorae <ganendra.wishnu@gmail.com>
2025-08-03 17:33:18 +0700

    change api to server

--- api/src/index.js
+++ api/src/index.js
@@ -11,7 +11,7 @@ async function main() {
     // Jalankan server di port yang ditentukan
     await new Promise((resolve) => httpServer.listen({ port: config.port }, resolve));
 
-    console.log(`🚀 Server API siap di http://localhost:${config.port}/graphql`);
+    console.log(`🚀 Server API siap di http://100.92.191.4:${config.port}/graphql`);
   } catch (error) {
     console.error("💥 Gagal memulai server API:", error);
     process.exit(1); // Keluar dari proses jika ada error fatal saat startup
